07f70c41e82197e16491a91768045dc14bffd222
strubium <113206902+strubium@users.noreply.github.com>
2024-04-09 15:45:16 -0500

    baller

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,9 +2,8 @@ import core.GameEngine;
 
 public class Application {
 
-
     public static void main(String[] args) {
-        new GameEngine().run("the game!");
+        GameEngine.run("the game!");
     }
 
 }
