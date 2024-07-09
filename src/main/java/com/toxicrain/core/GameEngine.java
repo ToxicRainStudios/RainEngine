630bd631da952c481c363d00d7bc6d8d82836332
strubium <113206902+strubium@users.noreply.github.com>
2024-07-08 19:52:10 -0500

    Cleanup GameEngine.java

--- src/main/java/com/toxicrain/core/GameEngine.java
+++ src/main/java/com/toxicrain/core/GameEngine.java
@@ -33,7 +33,6 @@ public class GameEngine {
 
     private static boolean fullscreen = false;
 
-
     public static void run(String windowTitle) {
         Logger.printLOG("Hello LWJGL " + Version.getVersion() + "!");
         Logger.printLOG("Hello RainEngine " + Constants.engineVersion + "!");
@@ -53,39 +52,34 @@ public class GameEngine {
     }
 
     private static void init(String windowTitle, boolean vSync) {
-        // Setup an error callback. The default implementation
-        // will print the error message in System.err.
+        // Setup an error callback. The default implementation will print the error message in System.err.
         GLFWErrorCallback.createPrint(System.err).set();
 
         Logger.printLOG("Initializing GLFW");
         // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if (!glfwInit())
-            throw new IllegalStateException("Unable to initialize GLFW");
+        if (!glfwInit()) throw new IllegalStateException("Unable to initialize GLFW");
 
         // Configure GLFW
         glfwDefaultWindowHints(); // the current window hints are already the default
         glfwWindowHint(GLFW_VISIBLE, 0); // the window will stay hidden after creation
         glfwWindowHint(GLFW_RESIZABLE, 1); // the window will be resizable
 
-        // Starts a new JVM if the application was started on macOS without the
-        // -XstartOnFirstThread argument.
+        // Starts a new JVM if the application was started on macOS without the -XstartOnFirstThread argument.
         if (startNewJvmIfRequired()) {
             System.exit(0);
         }
 
-        Logger.printLOG("Creating the game window");
+        Logger.printLOG("Creating Game Window");
         // Create the window
         window = glfwCreateWindow(300, 300, windowTitle, glfwGetPrimaryMonitor(), NULL);
         // Resize the window
         glfwSetWindowSize(window, (int) Constants.windowWidth, (int) Constants.windowHeight);
 
-        if (window == NULL)
-            throw new RuntimeException("Failed to create the GLFW window");
+        if (window == NULL) throw new RuntimeException("Failed to create the GLFW window");
 
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
         glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                glfwSetWindowShouldClose(window, true); // This is detected in the rendering loop
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) glfwSetWindowShouldClose(window, true); // This is detected in the rendering loop
             if (glfwGetKey(window, GLFW_KEY_F11) == GLFW_PRESS) {
                 toggleFullscreen();
             }
@@ -103,44 +97,30 @@ public class GameEngine {
             GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
             // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
+            glfwSetWindowPos(window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2);
         } // the stack frame is popped automatically
 
         // Make the OpenGL context current
         glfwMakeContextCurrent(window);
 
         // Enable v-sync
-        if (vSync) {
-            glfwSwapInterval(1);
-            glfwShowWindow(window);
-        } else {
-            glfwSwapInterval(0);
-            glfwShowWindow(window);
-        }
+        glfwSwapInterval(vSync ? 1 : 0);
+        glfwShowWindow(window);
 
         GL.createCapabilities();
         Logger.printLOG("Init Textures");
         TextureUtil.initTextures();
     }
 
-
     private static void loop() {
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
+        // This line is critical for LWJGL's interoperation with GLFW's OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread, creates the GLCapabilities instance and makes the OpenGL bindings available for use.
         GL.createCapabilities();
 
         // Set the clear color
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-        // Run the rendering loop until the user has attempted to close
-        // the window/pressed the ESCAPE key.
+        // Run the rendering loop until the user has attempted to close the window/pressed the ESCAPE key.
         while (!glfwWindowShouldClose(window)) {
             // Set the viewport size
             glViewport(0, 0, 1920, 1080);
@@ -152,16 +132,9 @@ public class GameEngine {
             glMatrixMode(GL_PROJECTION);
             glLoadMatrixf(createPerspectiveProjectionMatrix(90.0f, 1920.0f / 1080.0f, 1.0f, 100.0f));
 
-            // Set up the projection matrix
-            glMatrixMode(GL_MODELVIEW);
-            glLoadIdentity();
-
-            glFrustum(-1, 1, -1, 1, 1, 100);
-
             // Set up the view matrix
             glMatrixMode(GL_MODELVIEW);
             glLoadIdentity();
-            // Translate the camera
             glTranslatef(-cameraX, -cameraY, -cameraZ);
 
             // Enable depth testing
@@ -208,20 +181,13 @@ public class GameEngine {
         glDisable(GL_TEXTURE_2D);
     }
 
-
     private static void processInput() {
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-            cameraZ -= cameraSpeed;
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-            cameraZ += cameraSpeed;
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-            cameraX -= cameraSpeed;
-        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-            cameraX += cameraSpeed;
-        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-            cameraY -= cameraSpeed;
-        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-            cameraY += cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) cameraZ -= cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) cameraZ += cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) cameraX -= cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) cameraX += cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) cameraY -= cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) cameraY += cameraSpeed;
     }
 
     /**
@@ -284,5 +250,4 @@ public class GameEngine {
         buffer.put(projectionMatrix).flip();
         return buffer;
     }
-
 }
