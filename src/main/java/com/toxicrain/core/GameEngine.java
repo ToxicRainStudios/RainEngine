4df5b673745aabf2b15397282c8b706d1c7f4c0d
strubium <113206902+strubium@users.noreply.github.com>
2024-07-21 16:37:05 -0500

    Move createCapabilities() and glClearColor() to init

--- src/main/java/com/toxicrain/core/GameEngine.java
+++ src/main/java/com/toxicrain/core/GameEngine.java
@@ -141,17 +141,16 @@ public class GameEngine {
         enableBlending();
     }
     */
-    }
-
-
-    private static void loop() {
         // This line is critical for LWJGL's interoperation with GLFW's OpenGL context, or any context that is managed externally.
         // LWJGL detects the context that is current in the current thread, creates the GLCapabilities instance and makes the OpenGL bindings available for use.
         GL.createCapabilities();
 
         // Set the clear color
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    }
 
+
+    private static void loop() {
         // Create the batch renderer
         BatchRenderer batchRenderer = new BatchRenderer();
 
