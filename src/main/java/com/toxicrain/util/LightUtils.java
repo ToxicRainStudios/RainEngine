25295f851fb02ad5f0a036bb12829916e1a9b2d2
strubium <113206902+strubium@users.noreply.github.com>
2024-07-29 15:43:31 -0500

    Javadoc for light system

--- src/main/java/com/toxicrain/util/LightUtils.java
+++ src/main/java/com/toxicrain/util/LightUtils.java
@@ -6,8 +6,14 @@ import java.util.List;
 public class LightUtils {
     private static List<float[]> lightSources = new ArrayList<>();
 
-    public static void addLightSource(float x, float y, float maxDistance) {
-        lightSources.add(new float[] { x, y, maxDistance });
+    /**
+     * Adds a light source to a position with a strength
+     * @param x the x position for the light
+     * @param y the y position for the light
+     * @param strength the strength of the light, cannot be 0 or 1
+     */
+    public static void addLightSource(float x, float y, float strength) {
+        lightSources.add(new float[] { x, y, strength });
     }
 
     public static List<float[]> getLightSources() {
