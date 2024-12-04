ab1934edbe382ef51807350980bf9bdcd7dfca1f
strubium <113206902+strubium@users.noreply.github.com>
2024-12-04 17:31:25 -0600

    some slight updates

--- src/main/java/com/toxicrain/Application.java
+++ src/main/java/com/toxicrain/Application.java
@@ -1,3 +1,5 @@
+package com.toxicrain;
+
 import com.toxicrain.core.RainLogger;
 import com.toxicrain.core.json.GameInfoParser;
 import com.toxicrain.core.GameEngine;
@@ -7,7 +9,7 @@ import java.util.Arrays;
 public class Application {
 
     public static void main(String[] args) {
-        RainLogger.printLOG("Starting game with: " + Arrays.toString(args));
+        RainLogger.printLOG("Starting with args: " + Arrays.toString(args));
 
         GameInfoParser.loadGameInfo();
         GameEngine.run();
